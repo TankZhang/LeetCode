7b34a47f188c4b015b89bb68e6c723060b7b2c54
Tank Zhang <tankzhang9712@outlook.com>
2016-07-18 20:43:28 +0800

    更新149简单方法，没有debug真的得不到正确代码。。。

--- 149MaxPointsonaLine/149MaxPointsonaLine.cpp
+++ 149MaxPointsonaLine/149MaxPointsonaLine.cpp
@@ -10,6 +10,41 @@
 	int y;
  };
 
+ //网上淘来超简单方法
+ int maxPoints2(struct Point* points, int pointsSize)
+ {
+	 if (pointsSize < 3)return pointsSize;
+	 int max = 2;
+	 for (int i = 0; i < pointsSize; i++)
+	 {
+		 int m = 0;
+		 int n = 0;
+		 for (int j = 0; j < pointsSize; j++)
+		 {
+			 if (points[j].x == points[i].x&&points[j].y == points[i].y)
+				 m++;
+		 }
+		 max = max > m ? max : m;
+		 for (int j = i+1; j < pointsSize; j++)
+		 {
+			 int x = points[j].x - points[i].x;
+			 int y= points[j].y - points[i].y;
+			 if (x == 0 && y == 0)
+				 continue;
+			 n = 1;
+			 for (int k = j+1; k < pointsSize; k++)
+			 {
+				 if (points[k].y - points[i].y == 0 && points[k].x - points[i].x == 0)continue;
+				 if (x*(points[k].y - points[i].y) == y*(points[k].x - points[i].x))
+					 n++;
+			 }
+			 max = max >(m + n) ? max : m + n;
+		 }
+	 }
+	 return max;
+ }
+
+ //自己的超级复杂方法
  int maxPoints(struct Point* points, int pointsSize) {
 	 if (pointsSize < 3)return pointsSize;
 	 int n = pointsSize;
@@ -57,18 +92,18 @@
 int main()
 {
 	Point points[10];
-	points->x = -4;
+	points->x = 1;
 	points->y = 1;
-	(points + 1)->x = -7;
-	(points + 1)->y = 7;
-	(points + 2)->x = -1;
-	(points + 2)->y = 5;
-	(points + 3)->x = 9;
-	(points + 3)->y = -25;
+	(points + 1)->x = 1;
+	(points + 1)->y = 1;
+	(points + 2)->x = 1;
+	(points + 2)->y = 1;
+	(points + 3)->x = 2;
+	(points + 3)->y = 2;
 	(points + 4)->x = 9;
 	(points + 4)->y = 651;
 
-	printf("%d\n", maxPoints(points, 4));
+	printf("%d\n", maxPoints2(points, 3));
 	getchar();
     return 0;
 }
