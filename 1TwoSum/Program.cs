2a4e485603635582e6b88d04b6bdfd79821f1670
Tank Zhang <tankzhang9712@outlook.com>
2016-07-21 17:25:19 +0800

    更新第1题答案

--- 1TwoSum/Program.cs
+++ 1TwoSum/Program.cs
@@ -11,9 +11,9 @@ namespace _1TwoSum
     {
         static void Main(string[] args)
         {
-            int[] nums = new int[] {3,2,4};
+            int[] nums = new int[] { 3, 2, 4 };
             int target = 6;
-            int[] result = TwoSum(nums, target);
+            int[] result = TwoSum2(nums, target);
             Console.WriteLine("{0}  {1}", result[0], result[1]);
             Console.ReadKey();
 
@@ -21,12 +21,12 @@ namespace _1TwoSum
 
         static int[] TwoSum(int[] nums, int target)
         {
-            int[] returns=new int[2];
+            int[] returns = new int[2];
             for (int i = 0; i < nums.Length; i++)
             {
-                for (int j = i+1; j < nums.Length; j++)
+                for (int j = i + 1; j < nums.Length; j++)
                 {
-                    if(nums[i]+nums[j]==target)
+                    if (nums[i] + nums[j] == target)
                     {
                         returns[0] = i;
                         returns[1] = j;
@@ -36,5 +36,38 @@ namespace _1TwoSum
             }
             return returns;
         }
+
+        //网上时间复杂度为O(n)的解法
+        static int[] TwoSum2(int[] nums, int target)
+        {
+            int[] returns = new int[2];
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i]))
+                {
+                    returns[0] = dic[nums[i]];
+                    returns[1] = i;
+                    return returns;
+                }
+                if (dic.ContainsKey(target - nums[i]))
+                {
+                    if (2 * (target - nums[i]) == target)
+                    {
+                        returns[0] = dic[nums[i]];
+                        returns[1] = i;
+                        return returns;
+
+                    }
+                }
+                else
+                {
+                    dic.Add(target - nums[i], i);
+                }
+            }
+            return returns;
+        }
+
+
     }
 }
