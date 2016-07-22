1d59247ebbbb293acdae64e838a5fa50d6239ed9
Tank Zhang <tankzhang9712@outlook.com>
2016-07-22 16:50:35 +0800

    添加225题目

---
+++ 225ImplementStackusingQueues/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//
+namespace _225ImplementStackusingQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Stack
+    {
+        // Push element x onto stack.
+        public void Push(int x)
+        {
+            qa.Enqueue(x);
+            while (qb.Count > 0)
+                qa.Enqueue(qb.Dequeue());
+            while (qa.Count > 0)
+                qb.Enqueue(qa.Dequeue());
+        }
+
+        // Removes the element on top of the stack.
+        public void Pop()
+        {
+            qb.Dequeue();
+        }
+
+        // Get the top element.
+        public int Top()
+        {
+            return qb.Peek();
+        }
+
+        // Return whether the stack is empty.
+        public bool Empty()
+        {
+            return qb.Count > 0 ? false : true;
+        }
+
+        Queue<int> qa = new Queue<int>();
+        Queue<int> qb = new Queue<int>();
+    }
+}
