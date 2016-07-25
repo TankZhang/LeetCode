f5e5cfce1f47e835c46447dc9bf287e3618e5073
Tank Zhang <tankzhang9712@outlook.com>
2016-07-25 16:52:22 +0800

    添加72题目简单办法

--- 72EditDistance/Program.cs
+++ 72EditDistance/Program.cs
@@ -11,12 +11,13 @@ namespace _72EditDistance
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(MinDistance("dinitrophenylhydrazine", "acetylphenylhydrazine"));
-            Console.WriteLine(MinDistance("", ""));
+            Console.WriteLine(MinDistance2("dinitrophenylhydrazine", "acetylphenylhydrazine"));
+            Console.WriteLine(MinDistance2("", ""));
 
             Console.ReadKey();
         }
 
+        //自己方法
         static int MinDistance(string word1, string word2)
         {
             int[,] nums = new int[word1.Length+1, word2.Length+1];
@@ -32,7 +33,7 @@ namespace _72EditDistance
             return distance;
 
         }
-
+        //自己方法
         private static int Go(string word1, string word2, int[,] nums)
         {
             if (word1 == "" && word2 == "")
@@ -77,5 +78,41 @@ namespace _72EditDistance
                 return d;
             }
         }
+
+        //简单办法dp
+        static int MinDistance2(string word1, string word2)
+        {
+            int length1 = word1.Length;
+            int length2 = word2.Length;
+            if (length1 == 0)
+                return length2;
+            if (length2 == 0)
+                return length1;
+            int[,] nums = new int[length1 + 1, length2 + 1];
+            for (int i = 0; i <=length1; i++)
+            {
+                nums[i, 0] = i;
+            }
+            for (int i = 0; i <=length2 ; i++)
+            {
+                nums[0, i] = i;
+            }
+
+            for (int i = 1; i <= length1; i++)
+            {
+                for (int j = 1; j <=length2; j++)
+                {
+                    if (word1[i - 1] == word2[j - 1])
+                        nums[i, j] = nums[i - 1, j - 1];
+                    else
+                    {
+                        nums[i, j] = Math.Min(Math.Min(nums[i - 1, j], nums[i, j - 1]), nums[i - 1, j - 1]) + 1;
+                    }
+                }
+            }
+
+            return nums[length1, length2];
+        }
+
     }
 }
