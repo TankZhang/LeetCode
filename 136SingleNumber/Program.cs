2289c6eb9cb47d735216cf439f17db7a87f68cb6
Tank Zhang <tankzhang9712@outlook.com>
2016-07-30 17:00:42 +0800

    添加136题目

---
+++ 136SingleNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://leetcode.com/problems/single-number/
+namespace _136SingleNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 3, 6, 8, 6, 3, 1 };
+            Console.WriteLine(SingleNumber(nums));
+            Console.ReadKey();
+        }
+        static int SingleNumber(int[] nums)
+        {
+            int temp = 0; ;
+            foreach (int item in nums)
+            {
+                temp ^= item;
+            }
+            return temp;
+        }
+    }
+}
