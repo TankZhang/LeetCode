c86a02f9b71e86681c37e10cac00bac2eac08691
Tank Zhang <tankzhang9712@outlook.com>
2016-09-01 21:55:17 +0800

    添加蛇形矩阵

--- 000Test/Program.cs
+++ 000Test/Program.cs
@@ -10,21 +10,44 @@ namespace _000Test
     {
         static void Main(string[] args)
         {
-            int[,] mat= new int[,] { { 1,2 }, { 2,3 }, { 3,4 } };
-            int[,] res = Go(true, mat);
-            int r = mat.GetLength(0);
-            int c = mat.GetLength(1);
-            for (int i = 0; i < c; i++)
-            {
-                for (int j = 0; j < r; j++)
-                {
-                    Console.Write("{0}  ",res[i, j]);
-                }
-                Console.WriteLine();
-            }
-
+            int a;
+            Int32.TryParse(Console.ReadLine(), out a);
+            SneakMatrix(a);
             Console.ReadKey();
         }
+        
+        //蛇形矩阵Go
+            static void SneakMatrix(int n)
+            {
+                int[,] temp = new int[n, n];
+                int pNow = 1;
+                for (int i = 1; i < 2 * n; i++)
+                {
+                    switch (i % 4)
+                    {
+                        case 1:
+                            for (int j = i / 4, k = i / 4; k < n - i / 4; k++)
+                                temp[j, k] = pNow++;
+                            break;
+                        case 2:
+                            for (int j = i / 4 + 1, k = n - 1 - i / 4; j < n - i / 4; j++)
+                                temp[j, k] = pNow++;
+                            break;
+                        case 3:
+                            for (int j = n - 1 - i / 4, k = n - 2 - i / 4; k > i / 4 - 1; k--)
+                                temp[j, k] = pNow++;
+                            break;
+                        case 0:
+                            for (int j = n -1- i / 4, k = i / 4-1; j > i / 4 - 1; j--)
+                                temp[j, k] = pNow++;
+                            break;
+                    }
+                }
+                for (int i = 0; i < n * n; i++)
+                {
+                    System.Console.Write("{0} ", temp[i / n, i % n]);
+                }
+            }
 
         static int[,] Go(bool flag,int[,] mat)
         {
