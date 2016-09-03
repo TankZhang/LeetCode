20e2c8c3101456d84438950ef6458427d4da4a9e
Tank Zhang <tankzhang9712@outlook.com>
2016-09-03 15:08:14 +0800

    添加279题目

---
+++ 279PerfectSquares/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://leetcode.com/problems/perfect-squares/
+namespace _279PerfectSquares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumSquares(12));
+            Console.ReadKey();
+        }
+        static int NumSquares(int n)
+        {
+            int[] temp = new int[n + 1];
+            for (int i = 0; i < n + 1; i++)
+                temp[i] = i;
+            for (int i = 0; i < n+1; i++)
+                for (int j = 1; j*j <= i; j++)
+                    temp[i] = Math.Min(temp[i], temp[i - j * j] + 1);
+            return temp[n];
+        }
+    }
+}
