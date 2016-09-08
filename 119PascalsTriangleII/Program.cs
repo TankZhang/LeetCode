c8b31ef3770ff8a7a7f6f9f04cb33dd0532d2efc
Tank Zhang <tankzhang9712@outlook.com>
2016-09-08 09:43:55 +0800

    添加119题目

---
+++ 119PascalsTriangleII/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _119PascalsTriangleII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int item in GetRow(4))
+                Console.Write(item + " ");
+
+            Console.ReadKey();
+        }
+
+        static IList<int> GetRow(int rowIndex)
+        {
+            IList<int> res = new List<int> { 1 };
+            if (rowIndex == 0)
+                return res;
+            res.Add(1);
+            if (rowIndex == 1)
+                return res;
+            int temp1 = 1,temp2;
+            for (int i = 1; i < rowIndex; i++)
+            {
+                for (int j = 1; j < i + 1; j++)
+                {
+                    temp2 = res[j];
+                    res[j] = res[j] + temp1;
+                    temp1 = temp2;
+                }
+                res.Add(1);
+            }
+            return res;
+        }
+    }
+}
