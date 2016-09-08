b4ecb0187e34ff95571a92afd6396771bbee59f2
Tank Zhang <tankzhang9712@outlook.com>
2016-09-08 10:00:08 +0800

    添加342题目

---
+++ 342PowerofFour/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _342PowerofFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPowerOfFour(-2147483648));
+            Console.ReadLine();
+        }
+        static bool IsPowerOfFour(int num)
+        {
+            if (num == 1)
+                return true;
+            string s = Convert.ToString(num, 2);
+            if (s.Length % 2 == 1 && s[0] == '1' && Convert.ToInt32(s.Substring(1), 2) == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
