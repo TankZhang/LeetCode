d412aa6764b589c8a963bedf1da6ad9a535ced22
Tank Zhang <tankzhang9712@outlook.com>
2016-09-10 16:57:55 +0800

    更新393方法

--- 393UTF-8Validation/Program.cs
+++ 393UTF-8Validation/Program.cs
@@ -16,34 +16,72 @@ namespace _393UTF_8Validation
             Console.ReadKey();
         }
 
+        //新方法
+        static bool ValidUtf8_1(int[] data)
+        {
+            try
+            {
+                for (int i = 0; i < data.Length; i++)
+                {
+                    string s = Convert.ToString(data[i], 2);
+                    if (s.Length == 8)
+                    {
+                        if (s.StartsWith("11110"))
+                            if (((data[++i] & 192) == 128) && ((data[++i] & 192) == 128) && ((data[++i] & 192) == 128))
+                                continue;
+                            else
+                                return false;
+                        if (s.StartsWith("1110"))
+                            if (((data[++i] & 192) == 128) && ((data[++i] & 192) == 128))
+                                continue;
+                            else
+                                return false;
+                        if (s.StartsWith("110"))
+                            if (((data[++i] & 192) == 128))
+                                continue;
+                            else
+                                return false;
+                        return false;
+                    }
+                }
+                return true;
+            }
+            catch { return false; }
+        }
+
         static bool ValidUtf8(int[] data)
         {
-            for (int i = 0; i < data.Length; i++)
+            try
             {
-                if (data[i] >= 248)
-                    return false;
-                else if (data[i] >= 240)
+                for (int i = 0; i < data.Length; i++)
                 {
-                    if (!IsInSection(data[++i],128,192)|| !IsInSection(data[++i], 128, 192) || !IsInSection(data[++i], 128, 192))
+                    if (data[i] >= 248)
+                        return false;
+                    else if (data[i] >= 240)
+                    {
+                        if (!IsInSection(data[++i], 128, 192) || !IsInSection(data[++i], 128, 192) || !IsInSection(data[++i], 128, 192))
+                            return false;
+                    }
+                    else if (data[i] >= 224)
+                    {
+                        if (!IsInSection(data[++i], 128, 192) || !IsInSection(data[++i], 128, 192))
+                            return false;
+                        Console.WriteLine(Convert.ToString(data[i], 2));
+                    }
+                    else if (data[i] >= 192)
+                    {
+                        if (!IsInSection(data[++i], 128, 192))
+                            return false;
+                    }
+                    else if (data[i] >= 128)
                         return false;
                 }
-                else if (data[i] >= 224)
-                {
-                    if (!IsInSection(data[++i], 128, 192) || !IsInSection(data[++i], 128, 192))
-                        return false;
-                    Console.WriteLine(Convert.ToString(data[i], 2));
-                }
-                else if (data[i] >= 192)
-                {
-                    if (!IsInSection(data[++i], 128, 192))
-                        return false;
-                }
-                else if (data[i] >= 128)
-                    return false;
+                return true;
             }
-            return true;
+            catch { return false; }
         }
-        static bool IsInSection(int data,int low ,int high)
+        static bool IsInSection(int data, int low, int high)
+
         { return data >= low && data < high; }
     }
 }
