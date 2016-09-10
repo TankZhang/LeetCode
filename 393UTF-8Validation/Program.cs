84b07a52da8b3d3d4e4c0ce2c541dad780c82366
Tank Zhang <tankzhang9712@outlook.com>
2016-09-10 16:43:35 +0800

    增加393题目

---
+++ 393UTF-8Validation/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://leetcode.com/problems/utf-8-validation/
+namespace _393UTF_8Validation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[] { 235, 140, 4 };
+            Console.WriteLine(ValidUtf8(data));
+            Console.ReadKey();
+        }
+
+        static bool ValidUtf8(int[] data)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] >= 248)
+                    return false;
+                else if (data[i] >= 240)
+                {
+                    if (!IsInSection(data[++i],128,192)|| !IsInSection(data[++i], 128, 192) || !IsInSection(data[++i], 128, 192))
+                        return false;
+                }
+                else if (data[i] >= 224)
+                {
+                    if (!IsInSection(data[++i], 128, 192) || !IsInSection(data[++i], 128, 192))
+                        return false;
+                    Console.WriteLine(Convert.ToString(data[i], 2));
+                }
+                else if (data[i] >= 192)
+                {
+                    if (!IsInSection(data[++i], 128, 192))
+                        return false;
+                }
+                else if (data[i] >= 128)
+                    return false;
+            }
+            return true;
+        }
+        static bool IsInSection(int data,int low ,int high)
+        { return data >= low && data < high; }
+    }
+}
