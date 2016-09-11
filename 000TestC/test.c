07f63008e21359e28f53330dc313c2468f0dfa99
Tank Zhang <tankzhang9712@outlook.com>
2016-09-11 12:08:54 +0800

    添加字符串排序

--- 000TestC/test.c
+++ 000TestC/test.c
@@ -58,11 +58,141 @@ int _m(char * str1, char * str2)
 	return 0;
 }
 
+void Change()
+{
+	char src[] = "zhegeG29aEd";
+	int n = strlen(src);
+	char* des = (char *)malloc(3 * n * sizeof(char));
+	int j = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (src[i] == 'a')
+		{
+			des[j++] = '#';
+			des[j++] = '*';
+			des[j++] = '#';
+		}
+		else if (src[i] <= '9' && src[i] >= '0')
+			continue;
+		else if (src[i] >= 'A'&&src[i] <= 'Z')
+		{
+			des[j++] = src[i] + 32;
+		}
+		else
+		{
+			des[j++] = src[i];
+		}
+	}
+	for (int i = 0; i < j; i++)
+	{
+		printf("%c", des[i]);
+	}
+}
+
+void SortText()
+{
+	char *strIn = gets();
+	int count = 1;
+	for (int i = 0; i < strlen(strIn); i++)
+		if (strIn[i] == ' ')
+			count++;
+	char** pStart = (char **)malloc(count * sizeof(char *));
+	int *charNum = (int *)malloc(count * sizeof(int));
+	int j = 0;
+	for (int i = 0; i < strlen(strIn); i++)
+	{
+		if (strIn[i] == ' ')
+			continue;
+		int k = 0;
+		while (strIn[i] != ' '&&i < strlen(strIn))
+		{k++; i++;}
+		charNum[j++] = k;
+	}
+
+	j = 0;
+	for (int i = 0; i < strlen(strIn); i++)
+	{
+		if (strIn[i] == ' ')
+			continue;
+		int k = 0;
+		char* temp = (char*)malloc(sizeof(char)*(charNum[j] + 1));
+		while (strIn[i] != ' '&&i < strlen(strIn))
+			temp[k++] = strIn[i++];
+		temp[k] = '\0';
+		//printf("%s\n", temp);
+		pStart[j++] = temp;
+	}
+
+	for (int i = 0; i < count; i++)
+	{
+		for (int k = i + 1; k < count; k++)
+		{
+			//printf("%d\n", strcmp(pStart[i], pStart[k]));
+			if (strcmp(pStart[i], pStart[k]) > 0)
+			{
+				char* temp = pStart[i];
+				pStart[i] = pStart[k];
+				pStart[k] = temp;
+			}
+		}
+	}
+
+	printf("\n答案输出：\n");
+
+	for (int i = 0; i < count; i++)
+	{
+		printf("%s\n", pStart[i]);
+	}
+
+}
+
+void SortText2()
+{
+	char *strIn = gets();
+	int wordNum = 1;
+	int strLen = strlen(strIn);
+	for (int i = 0; i < strLen; i++)
+		if (strIn[i] == ' ')
+		{
+			strIn[i] = '\0';
+			wordNum++;
+		}
+	char** pStart = (char **)malloc(wordNum * sizeof(char *));
+	int j = 0;
+	for (int i = 0; i < wordNum; i++)
+	{
+		pStart[i] = &strIn[j];
+		j += (strlen(pStart[i]) + 1);
+
+		printf("%s\n", pStart[i]);
+	}
+	for (int i = 0; i < wordNum; i++)
+	{
+		for (int k = i + 1; k < wordNum; k++)
+		{
+			//printf("%d\n", strcmp(pStart[i], pStart[k]));
+			if (strcmp(pStart[i], pStart[k]) > 0)
+			{
+				char* temp = pStart[i];
+				pStart[i] = pStart[k];
+				pStart[k] = temp;
+			}
+		}
+	}
+
+	printf("\n答案输出：\n");
+
+	for (int i = 0; i < wordNum; i++)
+	{
+		printf("%s\n", pStart[i]);
+	}
+
+}
+
 int main(void)
 {
-	printf("%d \n", _m("hello world", "h*wo*l*"));
+	SortText2();
 	getchar();
-	return 0;
 }
 
 
