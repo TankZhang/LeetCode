ce13593d10951ab40a73cc5c3fada1e2efb23aea
TankZhang <13926855+TankZhang@users.noreply.github.com>
2017-11-01 22:27:50 +0800

    update 476

---
+++ 476NumberComplement/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//https://leetcode.com/problems/number-complement/description/
+
+namespace _476NumberComplement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(FindComplement(Convert.ToInt32(input)));
+            Console.ReadKey();
+        }
+
+        static int FindComplement(int num)
+        {
+            int result = 0;
+            Stack<int> bStack = new Stack<int>();
+            int yu = 0;
+            while (num != 0)
+            {
+                yu = num % 2;
+                bStack.Push(yu);
+                num /= 2;
+            }
+            while (bStack.Count != 0)
+            {
+                result += (int)((1-bStack.Pop())*Math.Pow(2,bStack.Count));
+            }
+            return result;
+        }
+    }
+}
