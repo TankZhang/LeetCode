fa5ce9e4602b26424bdf286fa95f31ab97f3d474
TankZhang <13926855+TankZhang@users.noreply.github.com>
2017-11-02 21:59:25 +0800

    update 191NumberOf1Bits

---
+++ 191NumberOf1Bits/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://leetcode.com/problems/number-of-1-bits/description/
+namespace _191NumberOf1Bits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HammingWeight(15));
+            Console.ReadKey();
+        }
+
+        static int HammingWeight(uint n)
+        {
+            uint result = 0;
+            do
+            {
+                result += (n%2);
+            } while ((n/=2)!=0);
+            return (int)result;
+        }
+    }
+}
