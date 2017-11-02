d09555a123d7e09f43f67d76a544fb35f169fada
TankZhang <13926855+TankZhang@users.noreply.github.com>
2017-11-02 21:49:09 +0800

    update 338CountingBits

---
+++ 338CountingBits/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://leetcode.com/problems/counting-bits/description/  
+namespace _338CountingBits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in CountBits(5))
+            {
+                Console.Write("{0},",item);
+            }
+            Console.ReadKey();
+        }
+        static  int[] CountBits(int num)
+        {
+            int[] result = new int[num + 1];
+            for (int i = 1; i <= num; i++)
+            {
+                result[i] = result[i / 2] + i % 2;
+            }
+            return result;
+        }
+    }
+}
