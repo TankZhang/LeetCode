56ee6573ebedfe1c95b6821aedea9b468d03f497
Tank Zhang <tankzhang9712@outlook.com>
2019-08-07 21:44:03 +0800

    just record

---
+++ 001TwoSumC/001TwoSumC.cpp
@@ -0,0 +1,34 @@
+﻿// 001TwoSumC.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+	int i, j;
+	int * arr;
+	*returnSize = 2;
+	for (i = 0; i < numsSize; i++)
+	{
+		for (j = i + 1; j < numsSize; j++)
+		{
+			if (target == nums[i] + nums[j])
+			{
+				arr = (int*)malloc((*returnSize) * sizeof(int));
+				arr[0] = i;
+				arr[1] = j;
+				return arr;
+			}
+		}
+	}
+	return NULL;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
+
+
+
